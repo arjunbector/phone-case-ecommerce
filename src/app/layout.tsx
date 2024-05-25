f832c85fff98f0027c177ba15ab9597c02471acf
Arjun Bector~ <arjunbector@gmail.com>
2024-05-25 23:50:12 +0530

    feat: add upload page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,10 +22,12 @@ export default function RootLayout({
       <head>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <body className={inter.className}>
-            <Navbar/>
-            {children}
-            <Footer/>
-            </body>
+            <Navbar />
+            <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+              <div className="flex-1 flex flex-col h-full">{children}</div>
+              <Footer />
+            </main>
+          </body>
         </ThemeProvider>
       </head>
     </html>
