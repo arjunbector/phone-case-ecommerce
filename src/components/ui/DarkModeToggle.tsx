ca313e075a1a64cc29db44163885e0e58cf821a9
Arjun Bector~ <arjunbector@gmail.com>
2024-05-26 14:04:04 +0530

    feat: add tooltip to dark mode toggle

--- src/components/ui/DarkModeToggle.tsx
+++ src/components/ui/DarkModeToggle.tsx
@@ -2,6 +2,12 @@
 import { useTheme } from "next-themes";
 import { buttonVariants } from "./button";
 import { Sun, Moon } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const DarkModeToggle = () => {
   const handleThemeChange = () => {
@@ -13,21 +19,29 @@ const DarkModeToggle = () => {
   };
   const { theme, setTheme } = useTheme();
   return (
-    <button
-      onClick={() => {
-        handleThemeChange();
-      }}
-      className={buttonVariants({
-        size: "sm",
-        variant: "ghost",
-      })}
-    >
-      {theme === "light" ? (
-        <Sun className="h-5 w-5" />
-      ) : (
-        <Moon className="h-5 w-5" />
-      )}
-    </button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <button
+            onClick={() => {
+              handleThemeChange();
+            }}
+            className={buttonVariants({
+              size: "sm",
+              variant: "ghost",
+            })}
+          >
+            {" "}
+            {theme === "light" ? (
+              <Sun className="h-5 w-5" />
+            ) : (
+              <Moon className="h-5 w-5" />
+            )}
+          </button>
+        </TooltipTrigger>
+        <TooltipContent>Toggle Dark Mode</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
